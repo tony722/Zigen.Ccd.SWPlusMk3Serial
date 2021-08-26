f69bcdb79bbfe495d5624bad5d5b75ad61e2422b
Tony Evert <tony722@gmail.com>
2021-08-26 15:53:47 -0700

    Initial Commit

---
+++ AET.Zigen.Ccd.SWPlusMkIIISerial/AET.Zigen.Ccd.SWPlusMkIIISerial/ZigenSWPlusMkIIISerial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Crestron.RAD.Common.Interfaces;
+using Crestron.RAD.Common.Transports;
+using Crestron.RAD.DeviceTypes.AudioVideoSwitcher;
+using Crestron.RAD.ProTransports;
+
+namespace AET.Zigen.Ccd.SWPlusMkIIISerial {
+  public class ZigenSWPlusMkIIISerial : AAudioVideoSwitcher, ISerialComport, ISimpl {
+
+    private void Initialize() {
+      ConnectionTransport.EnableLogging = InternalEnableLogging;
+      ConnectionTransport.EnableTxDebug = InternalEnableTxDebug;
+      ConnectionTransport.EnableRxDebug = InternalEnableRxDebug;
+      ConnectionTransport.CustomLogger = InternalCustomLogger;
+
+      AudioVideoSwitcherProtocol = new ZigenSWPlusMkIIISerialProtocol(ConnectionTransport, Id) {
+        EnableLogging = InternalEnableLogging,
+        CustomLogger = InternalCustomLogger
+      };
+      AudioVideoSwitcherProtocol.RxOut += SendRxOut;
+      AudioVideoSwitcherProtocol.Initialize(AudioVideoSwitcherData);
+
+      Connected = true;
+    }
+
+    public void Initialize(IComPort comPort) {
+      ConnectionTransport = new CommonSerialComport(comPort);
+      Initialize();
+      Connected = true;
+    }
+
+    public SimplTransport Initialize(Action<string, object[]> send) {
+      ConnectionTransport = new SimplTransport();
+      Initialize();
+      ConnectionTransport.Send = send;
+      return (SimplTransport)ConnectionTransport;
+    }
+
+  }
+}
---
+++ AET.Zigen.Ccd.SWPlusMkIIISerial/AET.Zigen.Ccd.SWPlusMkIIISerial/ZigenSWPlusMkIIISerialProtocol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Crestron.RAD.Common.BasicDriver;
+using Crestron.RAD.Common.Enums;
+using Crestron.RAD.Common.Transports;
+using Crestron.RAD.DeviceTypes.AudioVideoSwitcher;
+using Crestron.SimplSharp;
+
+namespace AET.Zigen.Ccd.SWPlusMkIIISerial {
+  public class ZigenSWPlusMkIIISerialProtocol : AAudioVideoSwitcherProtocol {
+
+    public ZigenSWPlusMkIIISerialProtocol(ISerialTransport transport, byte id)
+      : base(transport, id) {
+    }
+
+    protected override bool PrepareStringThenSend(CommandSet commandSet) {
+      if (EnableLogging) base.Log("Zigen SWPlus TX$: '" + commandSet.Command + "'");
+      commandSet.Command += "\n";
+      return base.PrepareStringThenSend(commandSet);
+    }
+
+  }
+}
